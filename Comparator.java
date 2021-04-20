60687c6cfd85f9d205e5b0b9ed7748edabe72479
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2021-04-20 18:01:15 -0400

    Add files via upload

---
+++ Comparator.java
@@ -0,0 +1,16 @@
+package prj5;
+/**
+ * An interface that is used to order certain objects 
+ * of our classes by comparing two objects 
+ * 
+ * @author Aniket Adhikari
+ * @version 04.19.2021 v1
+ * 
+ * @param <T> the type of objects that may be 
+ *            compared by this comparator
+ */
+public interface Comparator<T> {
+    
+    
+
+}
