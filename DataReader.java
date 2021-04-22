0a21f7f5d572e5748bd6b1bc9d7bfb96cd07d270
jennyntran <82841270+jennyntran@users.noreply.github.com>
2021-04-22 13:53:04 -0400

    Add files via upload

--- DataReader.java
+++ DataReader.java
@@ -1,4 +1,36 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import bsh.ParseException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+/**
+ * 
+ * @author Jenny Tran
+ * @version 2021.04.22
+ */
 public class DataReader {
 
+    private LinkedList<State> states;
+    
+    /**
+     * 
+     * @param fileName
+     */
+    public DataReader(String fileName)
+    {
+        
+    }
+    
+    /**
+     * 
+     * @param file
+     * @return
+     */
+    public LinkedList<State> readDataFile(String file)
+    {
+        
+    }
 }
