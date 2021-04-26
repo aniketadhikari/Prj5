da85b9f36bc957c6d12aaf9ec188cc4c6a1e8d87
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2021-04-26 14:19:43 -0400

    CompareCFRTest.java
    4/26/2021 2:00 PM

--- CompareCFRTest.java
+++ CompareCFRTest.java
@@ -1,92 +1,102 @@
 package prj5;
+
 import student.TestCase;
 
 /**
-* Test class for the CompareCFR class
-* 
-* @author Aniket Adhikari
-* @version 04.23.2021 v2
-*
-*/
+ * Test class for the CompareCFR class
+ * 
+ * @author Aniket Adhikari
+ * @version 04.23.2021 v2
+ *
+ */
 public class CompareCFRTest extends TestCase {
-    
+
     private Race alien;
     private Race human;
     private CompareCFR<Race> cfr;
-    
+
     /**
      * Runs before every execution
      */
-    public void setUp()
-    {
+    public void setUp() {
         cfr = new CompareCFR<Race>();
         alien = new Race("Alien", 120, 352);
         human = new Race("Human", 100, 500);
     }
-    
+
+
     /**
-     * tests the compare method for when we are 
-     * comparing the CFRs of two two races where 
+     * tests the compare method for when we are
+     * comparing the CFRs of two two races where
      * the 1st parameter has a greater CFR
      */
-    public void testComparePositive()
-    {
+    public void testComparePositive() {
         assertEquals(-1, cfr.compare(alien, human));
+
     }
-    
+
+
     /**
-     * tests the compare method for when we are 
-     * comparing the CFRs of two races where the 
-     * 2nd parameter has a greater CFR 
+     * Test for r1 > r2
      */
-    public void testCompareNegative()
-    {
+    public void testR1GreaterR2() {
+        Race r1 = new Race("r1", 99, 100);
+        Race r2 = new Race("r1", 1, 100);
+
+        assertEquals(-1, cfr.compare(r1, r2));
+
+    }
+
+
+    /**
+     * tests the compare method for when we are
+     * comparing the CFRs of two races where the
+     * 2nd parameter has a greater CFR
+     */
+    public void testCompareNegative() {
         assertEquals(1, cfr.compare(human, alien));
     }
-    
+
+
     /**
-     * tests the compare method for when we are 
-     * comparing the CFRs of two races where the 
+     * tests the compare method for when we are
+     * comparing the CFRs of two races where the
      * CFRs are equal
      */
-    public void testCompareEqual()
-    {
+    public void testCompareEqual() {
         Race alienCopy = new Race("Alien Copy", 120, 352);
         assertEquals(-5, cfr.compare(alien, alienCopy));
     }
-    
+
+
     /**
-     * tests the compare method when the first parameter 
+     * tests the compare method when the first parameter
      * is null
      */
-    public void testCompareFirstNull()
-    {
+    public void testCompareFirstNull() {
         Exception thrown = null;
-        try
-        {
+        try {
             cfr.compare(null, alien);
         }
-        catch(Exception e)
-        {
+        catch (Exception e) {
             thrown = e;
         }
         assertTrue(thrown instanceof IllegalArgumentException);
         assertEquals(thrown.getMessage(), "One of the parameters"
             + "has not been initialized");
     }
+
+
     /**
-     * tests the compare method when the first parameter 
+     * tests the compare method when the first parameter
      * is null
      */
-    public void testCompareSecondNull()
-    {
+    public void testCompareSecondNull() {
         Exception thrown = null;
-        try
-        {
+        try {
             cfr.compare(alien, null);
         }
-        catch(Exception e)
-        {
+        catch (Exception e) {
             thrown = e;
         }
         assertTrue(thrown instanceof IllegalArgumentException);
