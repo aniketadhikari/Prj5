e7d2fd2e93701cd07d2264381e57f6ec70f376b5
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2021-04-26 14:18:02 -0400

    Comparator.java
    4/26/2021 2:00 PM

--- Comparator.java
+++ Comparator.java
@@ -1,20 +1,25 @@
 package prj5;
+
 /**
- * An interface that is used to order certain objects 
- * of our classes by comparing two objects 
+ * An interface that is used to order certain objects
+ * of our classes by comparing two objects
  * 
  * @author Aniket Adhikari
- * @version 04.23.2021 
+ * @version 04.23.2021
  * 
- * @param <T> the type of objects that may be 
+ * @param <T>
+ *            the type of objects that may be
  *            compared by this comparator
  */
 public interface Comparator<T> {
-    
+
     /**
      * compares the arguments for order
-     * @param obj1 object being compared to obj2
-     * @param obj2 object being compared to obj1
+     * 
+     * @param obj1
+     *            object being compared to obj2
+     * @param obj2
+     *            object being compared to obj1
      * @return -1 if obj1 is less than obj2, 0 if
      *         they are equal to one another, and
      *         1 if obj1 is more than obj2
