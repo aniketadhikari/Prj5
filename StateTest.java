945df8eaccc627af4fc7263eeafd2b02764b8c39
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2021-04-26 14:22:10 -0400

    StateTest.java
    4/26/2021 2:00 PM

--- StateTest.java
+++ StateTest.java
@@ -3,6 +3,8 @@ package prj5;
 import student.TestCase;
 
 /**
+ * Tests methods within State Class
+ * 
  * @author Aniket Adhikari
  * @version 04.19.2021
  */
@@ -11,7 +13,7 @@ public class StateTest extends TestCase {
     private State virginia;
 
     /**
-     * runs before every execution
+     * Sets up test methods
      */
     public void setUp() {
         demographics = new LinkedList<Race>();
@@ -27,29 +29,30 @@ public class StateTest extends TestCase {
 
     /**
      * tests the toString method which lists the races
-     * in the state along with the cases and CFR 
+     * in the state along with the cases and CFR
      * associated with each race
      */
     public void testToString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("VA \n");
-        sb.append("asian: 500 cases, 50.0% CFR \n");
-        sb.append("black: 500 cases, 60.0% CFR \n");
-        sb.append("latinx: 500 cases, 30.0% CFR \n");
-        sb.append("other: 500 cases, 20.0% CFR \n");
-        sb.append("white: 500 cases, 40.0% CFR \n");
+        sb.append("VA\n");
+        sb.append("asian: 500 cases, 50% CFR \n");
+        sb.append("black: 500 cases, 60% CFR \n");
+        sb.append("latinx: 500 cases, 30% CFR \n");
+        sb.append("other: 500 cases, 20% CFR \n");
+        sb.append("white: 500 cases, 40% CFR \n");
         sb.append("===== \n");
-        sb.append("black: 500 cases, 60.0% CFR \n");
-        sb.append("asian: 500 cases, 50.0% CFR \n");
-        sb.append("white: 500 cases, 40.0% CFR \n");
-        sb.append("latinx: 500 cases, 30.0% CFR \n");
-        sb.append("other: 500 cases, 20.0% CFR \n");
+        sb.append("black: 500 cases, 60% CFR \n");
+        sb.append("asian: 500 cases, 50% CFR \n");
+        sb.append("white: 500 cases, 40% CFR \n");
+        sb.append("latinx: 500 cases, 30% CFR \n");
+        sb.append("other: 500 cases, 20% CFR \n");
         assertEquals(sb.toString(), virginia.toString());
 
     }
 
+
     /**
-     * tests the getStateName method 
+     * tests the getStateName method
      */
     public void testGetStateName() {
         assertEquals("VA", virginia.getStateName());
@@ -57,46 +60,55 @@ public class StateTest extends TestCase {
 
 
     /**
-     * tests the callSortAlpha method 
+     * tests the callSortAlpha method
      */
     public void testCallSortAlpha() {
         StringBuilder unsorted = new StringBuilder();
-        unsorted.append("{latinx: 500 cases, 30.0% CFR, ");
-        unsorted.append("black: 500 cases, 60.0% CFR, ");
-        unsorted.append("asian: 500 cases, 50.0% CFR, ");
-        unsorted.append("other: 500 cases, 20.0% CFR, ");
-        unsorted.append("white: 500 cases, 40.0% CFR}");
+        unsorted.append("{latinx: 500 cases, 30% CFR, ");
+        unsorted.append("black: 500 cases, 60% CFR, ");
+        unsorted.append("asian: 500 cases, 50% CFR, ");
+        unsorted.append("other: 500 cases, 20% CFR, ");
+        unsorted.append("white: 500 cases, 40% CFR}");
         assertEquals(demographics.toString(), unsorted.toString());
         StringBuilder sortedAlpha = new StringBuilder();
-        sortedAlpha.append("{asian: 500 cases, 50.0% CFR, ");
-        sortedAlpha.append("black: 500 cases, 60.0% CFR, ");
-        sortedAlpha.append("latinx: 500 cases, 30.0% CFR, ");
-        sortedAlpha.append("other: 500 cases, 20.0% CFR, ");
-        sortedAlpha.append("white: 500 cases, 40.0% CFR}");
+        sortedAlpha.append("{asian: 500 cases, 50% CFR, ");
+        sortedAlpha.append("black: 500 cases, 60% CFR, ");
+        sortedAlpha.append("latinx: 500 cases, 30% CFR, ");
+        sortedAlpha.append("other: 500 cases, 20% CFR, ");
+        sortedAlpha.append("white: 500 cases, 40% CFR}");
         virginia.callSortAlpha();
         assertEquals(demographics.toString(), sortedAlpha.toString());
-        
+
     }
-    
+
+
     /**
-     * tests the callSortCFR method 
+     * tests the callSortCFR method
      */
     public void testCallSortCFR() {
         StringBuilder unsorted = new StringBuilder();
-        unsorted.append("{latinx: 500 cases, 30.0% CFR, ");
-        unsorted.append("black: 500 cases, 60.0% CFR, ");
-        unsorted.append("asian: 500 cases, 50.0% CFR, ");
-        unsorted.append("other: 500 cases, 20.0% CFR, ");
-        unsorted.append("white: 500 cases, 40.0% CFR}");
+        unsorted.append("{latinx: 500 cases, 30% CFR, ");
+        unsorted.append("black: 500 cases, 60% CFR, ");
+        unsorted.append("asian: 500 cases, 50% CFR, ");
+        unsorted.append("other: 500 cases, 20% CFR, ");
+        unsorted.append("white: 500 cases, 40% CFR}");
         assertEquals(demographics.toString(), unsorted.toString());
         StringBuilder sortedCFR = new StringBuilder();
-        sortedCFR.append("{black: 500 cases, 60.0% CFR, ");
-        sortedCFR.append("asian: 500 cases, 50.0% CFR, ");
-        sortedCFR.append("white: 500 cases, 40.0% CFR, ");
-        sortedCFR.append("latinx: 500 cases, 30.0% CFR, ");
-        sortedCFR.append("other: 500 cases, 20.0% CFR}");
+        sortedCFR.append("{black: 500 cases, 60% CFR, ");
+        sortedCFR.append("asian: 500 cases, 50% CFR, ");
+        sortedCFR.append("white: 500 cases, 40% CFR, ");
+        sortedCFR.append("latinx: 500 cases, 30% CFR, ");
+        sortedCFR.append("other: 500 cases, 20% CFR}");
         virginia.callSortCFR();
         assertEquals(demographics.toString(), sortedCFR.toString());
+    }
+
+
+    /**
+     * tests if list of races for states is returned
+     */
+    public void testGetList() {
+        assertEquals(virginia.getRaceList(), demographics);
     }
 
 }
