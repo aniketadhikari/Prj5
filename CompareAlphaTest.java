1746869b7fbf1c4f7236f70d966a0d3308f13014
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2021-04-26 14:18:52 -0400

    CompareAlphaTest.java
    4/26/2021 2:00 PM

--- CompareAlphaTest.java
+++ CompareAlphaTest.java
@@ -1,101 +1,98 @@
 package prj5;
+
 import student.TestCase;
 
 /**
  * Test class for the CompareAlpha class
  * 
  * @author Aniket Adhikari
- * @version 04.23.2021 
+ * @version 04.23.2021
  *
  */
 public class CompareAlphaTest extends TestCase {
     private Race alien;
     private Race human;
     private CompareAlpha<Race> alpha;
-    
+
     /**
      * Runs before every execution
      */
-    public void setUp()
-    {
+    public void setUp() {
         alien = new Race("Alien", 100, 500);
         human = new Race("Human", 100, 500);
         alpha = new CompareAlpha<Race>();
     }
-    
+
+
     /**
      * tests the compare method when the first parameter's
-     * name comes before the second parameter's name 
+     * name comes before the second parameter's name
      * alphabetically
      */
-    public void testCompareNegative()
-    {
+    public void testCompareNegative() {
         assertEquals(-1, alpha.compare(alien, human));
-        
+
     }
-    
+
+
     /**
      * tests the compare method when the second parameter's
-     * name comes before the first parameter's name 
+     * name comes before the first parameter's name
      * alphabetically
      */
-    public void testComparePositive()
-    {
+    public void testComparePositive() {
         assertEquals(1, alpha.compare(human, alien));
     }
-    
+
+
     /**
-     * tests the compare method when we compare races 
+     * tests the compare method when we compare races
      * with the same exact name
      */
-    public void testCompareSame()
-    {
+    public void testCompareSame() {
         Race alienCopy = new Race("Alien", 0, 0);
         assertEquals(0, alpha.compare(alienCopy, alien));
     }
-    
+
+
     /**
-     * tests the compare method when we compare races 
-     * with the same name but different casing 
+     * tests the compare method when we compare races
+     * with the same name but different casing
      */
-    public void testCompareCases()
-    {
+    public void testCompareCases() {
         Race lowerCaseHuman = new Race("hUmAn", 100, 500);
         assertEquals(0, alpha.compare(human, lowerCaseHuman));
     }
-    
+
+
     /**
-     * tests the compare method when the first parameter 
+     * tests the compare method when the first parameter
      * is null
      */
-    public void testCompareFirstNull()
-    {
+    public void testCompareFirstNull() {
         Exception thrown = null;
-        try
-        {
+        try {
             alpha.compare(null, alien);
         }
-        catch(Exception e)
-        {
+        catch (Exception e) {
             thrown = e;
         }
         assertTrue(thrown instanceof IllegalArgumentException);
         assertEquals(thrown.getMessage(), "One of the parameters"
             + "has not been initialized");
     }
+
+
     /**
-     * tests the compare method when the first parameter 
+     * tests the compare method when the first parameter
      * is null
      */
-    public void testCompareSecondNull()
-    {
+    public void testCompareSecondNull() {
         Exception thrown = null;
-        try
-        {
+        try {
             alpha.compare(alien, null);
         }
-        catch(Exception e)
-        {
+        catch (Exception e) {
             thrown = e;
         }
         assertTrue(thrown instanceof IllegalArgumentException);
