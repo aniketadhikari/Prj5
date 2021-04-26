94a8d1631420a536dc5e692863d5ec8d02f9f374
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2021-04-26 14:18:24 -0400

    CompareAlpha.java
    4/26/2021 2:00 PM

--- CompareAlpha.java
+++ CompareAlpha.java
@@ -1,31 +1,33 @@
 package prj5;
+
 /**
- * implements the comparator class by providing 
- * the user with a way of comparing the names 
+ * implements the comparator class by providing
+ * the user with a way of comparing the names
  * of Race objects alphabetically
  * 
  * @author Aniket Adhikari
- * @version 04.23.2021 
+ * @version 04.23.2021
  * 
- * @param <T> the type of objects that may be 
+ * @param <T>
+ *            the type of objects that may be
  *            compared by this comparator
  */
 public class CompareAlpha<T> implements Comparator<T> {
     /**
      * 
-     * @param obj1 is the object who's name is being compared
-     *        alphabetically to obj2
-     * @param obj2 is the object who's name is being compared 
-     *        alphabetically to obj1
-     * @return -1 if, alphabetically, the first object's name 
-     *         comes before the second, 1 if the second object's 
+     * @param obj1
+     *            is the object who's name is being compared
+     *            alphabetically to obj2
+     * @param obj2
+     *            is the object who's name is being compared
+     *            alphabetically to obj1
+     * @return -1 if, alphabetically, the first object's name
+     *         comes before the second, 1 if the second object's
      *         name comes before the first, and 0 if they have
      *         the same name
      */
-    public int compare(T obj1, T obj2)
-    {
-        if (obj1 == null || obj2 == null)
-        {
+    public int compare(T obj1, T obj2) {
+        if (obj1 == null || obj2 == null) {
             throw new IllegalArgumentException("One of the parameters"
                 + "has not been initialized");
         }
@@ -35,19 +37,15 @@ public class CompareAlpha<T> implements Comparator<T> {
         String name2 = race2.getRace().toLowerCase();
         int val = name1.compareToIgnoreCase(name2);
         // name1 comes after name2
-        if (val > 0)
-        {
-            return 1; 
+        if (val > 0) {
+            return 1;
         }
         // name1 comes before name2
-        else if (val < 0)
-        {
+        else if (val < 0) {
             return -1;
         }
         // name is the same
         return 0;
-        
-        
-        
+
     }
 }
