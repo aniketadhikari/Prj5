96f3300977f017f08081c50de854404b6c4c6432
jennyntran <82841270+jennyntran@users.noreply.github.com>
2021-04-27 19:05:06 -0400

    Add files via upload

--- Input.java
+++ Input.java
@@ -1,16 +1,12 @@
-/**
- * 
- */
 package prj5;
 
 import java.io.FileNotFoundException;
-import java.util.Iterator;
 
 /**
  * Project Runner
  * 
- * @author Jenny Tran
- * @version 2021.04.23
+ * @author Aniket Adhikari, Jenny Tran, Sami Al Jadir
+ * @version 2021.04.27
  */
 public class Input {
 
@@ -32,13 +28,13 @@ public class Input {
             dR = new DataReader("Cases_and_Deaths_by_race_RANDOM_NUMBERS.csv");
 
         }
-//        LinkedList<State> states = dR.getState();
+// LinkedList<State> states = dR.getState();
 //
-//        Iterator<State> statesIter = states.iterator();
-//        while (statesIter.hasNext()) {
+// Iterator<State> statesIter = states.iterator();
+// while (statesIter.hasNext()) {
 //
-//            System.out.println(statesIter.next().toString());
-//        }
+// System.out.println(statesIter.next().toString());
+// }
     }
 
 }
